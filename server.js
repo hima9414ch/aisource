b20c31af80a30fc4a695ae8c9dee428890db7da6
hima9414ch <chauhan1232000@gmail.com>
2025-02-02 19:30:39 +0530

    Updating file from Flask backend

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-const express = require('express');const app = express();app.get('/', (req, res) => {res.send('Hello World!');});app.listen(3000, () => {console.log('Server is running on port 3000');});
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const app = express();
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+res.send('Hello, World! You are connected to the backend.');
+});
+app.post('/', async (req, res) => {
+const { name, email } = req.body;
+const apiUrl = 'https://aisource.vercel.app/';
+const response = await fetch(apiUrl, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name, email }) }).then(res => res.json());
+res.send(response);
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log('Server is running on port ' + PORT);
+});
