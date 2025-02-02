37438dc0084d09c5ee9bb2614411780ed4d8005b
hima9414ch <chauhan1232000@gmail.com>
2025-02-02 19:26:06 +0530

    Updating file from Flask backend

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-const express = require('express');const bodyParser = require('body-parser');const app = express();app.use(bodyParser.json());app.post('/api/payload', (req, res) => {const payload = req.body.payload;res.json({payload});});app.get('/', (req, res) => {res.send('Hello World!');});const PORT = process.env.PORT || 3000;app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
+const express = require('express');const app = express();app.get('/', (req, res) => {res.send('Hello World!');});app.listen(3000, () => {console.log('Server is running on port 3000');});
