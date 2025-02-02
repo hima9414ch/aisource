c4201bd363832374f18c852943801a346712b9a1
hima9414ch <chauhan1232000@gmail.com>
2025-02-02 19:00:50 +0530

    Updating file from Flask backend

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-const express = require('express'); const app = express(); app.get('/', (req, res) => { res.send('Hello World from User Portfolio!'); }); app.listen(3000, () => { console.log('Server is running on http://localhost:3000'); });
+const express = require('express');const app = express();app.get('/', (req, res) => { res.send('Hello World!'); });app.listen(3000, () => { console.log('Server is running on port 3000'); });
