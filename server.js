238394c4ea6a2655b71258ad372263f09399b70e
hima9414ch <chauhan1232000@gmail.com>
2025-02-03 23:43:31 +0530

    Updating file from Flask backend

--- server.js
+++ server.js
@@ -1,18 +1,1 @@
-// server.js
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-app.get('/', (req, res) => {
-res.send('Hello, World! You are connected to the backend.');
-});
-app.post('/', async (req, res) => {
-const { name, email } = req.body;
-
-res.send({name, email});
-});
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-console.log('Server is running on port ' + PORT);
-});
+const http = require('http'); const server = http.createServer((req, res) => { if (req.url === '/') { res.writeHead(200, { 'Content-Type': 'text/plain' }); res.end('Hello World! Welcome to the Real Estate Website'); } }); server.listen(3000, () => { console.log('Server is running on http://localhost:3000'); });
